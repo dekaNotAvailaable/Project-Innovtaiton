7390a1b9ca4d866ca5e2cffac458a4c6743431a4
dekaNotAvailaable <125357384+dekaNotAvailaable@users.noreply.github.com>
2024-02-14 19:45:39 +0100

    .

---
+++ Assets/MobileARTemplateAssets/Scripts/ColorDetection.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ColorDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void ColorDetectionByInput()
+    {
+        if (Input.touchCount > 0)
+        { Touch touch = Input.GetTouch(0); }
+
+
+    }
+}

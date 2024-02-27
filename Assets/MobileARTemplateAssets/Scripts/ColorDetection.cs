884c9fd66e39d613f9183c360e728e935d216910
dekaNotAvailaable <125357384+dekaNotAvailaable@users.noreply.github.com>
2024-02-27 11:55:45 +0100

    changes

--- Assets/MobileARTemplateAssets/Scripts/ColorDetection.cs
+++ Assets/MobileARTemplateAssets/Scripts/ColorDetection.cs
@@ -5,42 +5,46 @@ public class ColorDetection : MonoBehaviour
     // Start is called before the first frame update
     void Update()
     {
-        /*if (Input.GetMouseButton(0))
-        {
-            ScreenCapture.CaptureScreenshot("MyScreenShot.png");
-            Debug.Log("ScreenShot Taken");
-        }
-        // print (Input.mousePosition);
+        print("TOuch COunt:" + Input.touchCount);
     }
 
-    void moonrake()
-    {
-        mpos = Input.mousePosition;
-        var ray = Camera.main.ScreenPointToRay(mpos);
-        var cube : GameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.transform.localScale = Vector3(20, 20, 1);
-        cube.transform.position = Vector3(0, 0.5, 0);
-        cube.transform.LookAt(camera.main.transform, Vector3.up);
-        Destroy(cube.renderer.material);
-        sshot();
-
-    }
-
-    void sshot()
-    {
-        var tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-        yield WaitForEndOfFrame();
-        tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
-        tex.Apply();
-
-        var bla = tex.GetPixel(mpos.x, mpos.y);
-        print(bla);
 
 
-        //Destroy (tex);
 
-        //return tex;
-   */
-    }
+    /*if (Input.GetMouseButton(0))
+       {
+           ScreenCapture.CaptureScreenshot("MyScreenShot.png");
+           Debug.Log("ScreenShot Taken");
+       }
+       // print (Input.mousePosition);
+   }
 
+   void moonrake()
+   {
+       mpos = Input.mousePosition;
+       var ray = Camera.main.ScreenPointToRay(mpos);
+       var cube : GameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+       cube.transform.localScale = Vector3(20, 20, 1);
+       cube.transform.position = Vector3(0, 0.5, 0);
+       cube.transform.LookAt(camera.main.transform, Vector3.up);
+       Destroy(cube.renderer.material);
+       sshot();
+
+   }
+
+   void sshot()
+   {
+       var tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+       yield WaitForEndOfFrame();
+       tex.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+       tex.Apply();
+
+       var bla = tex.GetPixel(mpos.x, mpos.y);
+       print(bla);
+
+
+       //Destroy (tex);
+
+       //return tex;
+  */
 }

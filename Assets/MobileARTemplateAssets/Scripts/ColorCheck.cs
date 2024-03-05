6146a4a1a7640ce2004d42688bf6a56db5c4ba71
dekaNotAvailaable <125357384+dekaNotAvailaable@users.noreply.github.com>
2024-03-05 13:55:07 +0100

    wait

--- Assets/MobileARTemplateAssets/Scripts/ColorCheck.cs
+++ Assets/MobileARTemplateAssets/Scripts/ColorCheck.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColorCheck : MonoBehaviour
 {
@@ -8,17 +9,27 @@ public class ColorCheck : MonoBehaviour
     PoitionColor poitionColor;
     ColorDetection colorDetection;
     public TextMeshProUGUI debugText;
-
+    public Image progressBar;
+    public float tolerance = 0.05f;
     void Start()
     {
         poitionColor = FindObjectOfType<PoitionColor>();
         colorDetection = FindObjectOfType<ColorDetection>();
         debugText.gameObject.SetActive(false);
-    }
+        if (progressBar != null)
+        {
+            Debug.LogWarning("prgoress bar is found !!!!!!!!1");
+            progressBar.fillAmount = 0;
+        }
 
+    }
     public void CheckColor()
     {
         Color detectedColor = colorDetection.pixelColor;
+        Color closestPotionColor = GetClosestPotionColor(detectedColor);
+        float matchPercentage = CalculateMatchPercentage(detectedColor, closestPotionColor);
+        UpdateProgressBar(matchPercentage);
+        Debug.Log(matchPercentage);
         for (int i = 0; i < poitionColor.Potions.Length; i++)
         {
             Color potionColor = poitionColor.GetPotionColor(i);
@@ -37,12 +48,56 @@ public class ColorCheck : MonoBehaviour
         debugText.color = Color.red;
         debugText.text = "Detected color does not match any potion color.";
     }
+
+    Color GetClosestPotionColor(Color detectedColor)
+    {
+        Color closestColor = Color.white;
+        float closestDistance = Mathf.Infinity;
+
+        foreach (Image potion in poitionColor.Potions)
+        {
+            if (potion != null)
+            {
+                Color potionColor = potion.color;
+                float distance = ColorDistance(detectedColor, potionColor);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestColor = potionColor;
+                }
+            }
+        }
+        return closestColor;
+    }
+
+    float CalculateMatchPercentage(Color detectedColor, Color closestPotionColor)
+    {
+        float distance = ColorDistance(detectedColor, closestPotionColor);
+        float matchPercentage = Mathf.Clamp01(1 - (distance / tolerance));
+        return matchPercentage;
+    }
+
+    void UpdateProgressBar(float matchPercentage)
+    {
+        if (progressBar != null)
+        {
+            progressBar.fillAmount = matchPercentage;
+        }
+    }
+
     bool ColorApproximatelyEqual(Color color1, Color color2)
     {
-        float tolerance = 0.05f;
         bool redMatch = Mathf.Abs(color1.r - color2.r) <= tolerance;
         bool greenMatch = Mathf.Abs(color1.g - color2.g) <= tolerance;
         bool blueMatch = Mathf.Abs(color1.b - color2.b) <= tolerance;
         return redMatch && greenMatch && blueMatch;
+    }
+
+    float ColorDistance(Color color1, Color color2)
+    {
+        float redDiff = Mathf.Abs(color1.r - color2.r);
+        float greenDiff = Mathf.Abs(color1.g - color2.g);
+        float blueDiff = Mathf.Abs(color1.b - color2.b);
+        return (redDiff + greenDiff + blueDiff) / 3f;
     }
 }

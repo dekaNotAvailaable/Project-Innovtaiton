90f6bc523a05e9bf11b155fe6b6d302387127944
dekaNotAvailaable <125357384+dekaNotAvailaable@users.noreply.github.com>
2024-03-07 17:00:33 +0100

    photon stage 1

---
+++ Assets/MobileARTemplateAssets/Scripts/ConnectToServer.cs
@@ -0,0 +1,18 @@
+using UnityEngine.SceneManagement;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+}
---
+++ Assets/MobileARTemplateAssets/Scripts/RoomManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        Debug.Log("Connecting...");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Connected to Server");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        PhotonNetwork.JoinOrCreateRoom("test", null, null);
+        Debug.Log("We're connected in a room now");
+    }
+}

0606f7ff8fab24c56371f9556dd0c37ea240335e
mariogreculescu <mariogreculescu@gmail.com>
2024-03-08 12:10:55 +0100

    merge with

---
+++ Assets/Scripts/ToggleImage.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class ToggleImage : MonoBehaviour
+{
+    public Image ImageDisplayGameObject;
+    public Sprite ImageToActive;
+    public void ShowImage()
+    {
+        ImageDisplayGameObject.sprite = ImageToActive;
+    }
+}
